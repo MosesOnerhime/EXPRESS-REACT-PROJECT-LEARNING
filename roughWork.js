6b08031dde66137d4c83f251320357ce4277e720
MosesOnerhime <mosesonerhime11@gmail.com>
2025-04-11 10:40:14 +0100

    initial commit

---
+++ roughWork.js
@@ -0,0 +1,136 @@
+import React from "react";
+import "./App.css";
+
+export const IphonePro = () => {
+  return (
+    <div className="iphone-pro">
+      <div className="overlap-group-wrapper">
+        <div className="overlap-group">
+          <div className="main">
+            <div className="section">
+              <div className="div">
+                <div className="text-wrapper">Upload Project</div>
+
+                <div className="button">
+                  <div className="frame">
+                    <img
+                      className="img"
+                      alt="Frame"
+                      src="https://c.animaapp.com/LDbp6h8N/img/frame.svg"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="div-2">
+                <div className="div-3">
+                  <div className="span-wrapper">
+                    <div className="span">
+                      <div className="text-wrapper-2">1</div>
+                    </div>
+                  </div>
+
+                  <div className="text-wrapper-3">Details</div>
+                </div>
+
+                <div className="div-4">
+                  <div className="div-wrapper">
+                    <div className="span-2">
+                      <div className="text-wrapper-4">2</div>
+                    </div>
+                  </div>
+
+                  <div className="text-wrapper-5">Media</div>
+                </div>
+
+                <div className="div-5">
+                  <div className="div-wrapper">
+                    <div className="span-2">
+                      <div className="text-wrapper-4">3</div>
+                    </div>
+                  </div>
+
+                  <div className="text-wrapper-6">Review</div>
+                </div>
+              </div>
+
+              <div className="form">
+                <div className="div-6">
+                  <div className="div-7">
+                    <div className="frame-2">
+                      <div className="frame-3">
+                        <div className="label">
+                          <div className="text-wrapper-7">Description</div>
+                        </div>
+
+                        <div className="textarea">
+                          <div className="text-wrapper-8">
+                            Describe your work
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="frame-wrapper">
+                        <div className="frame-4">
+                          <div className="label">
+                            <div className="text-wrapper-9">Category</div>
+                          </div>
+
+                          <div className="select">
+                            <img
+                              className="frame-5"
+                              alt="Frame"
+                              src="https://c.animaapp.com/LDbp6h8N/img/frame-1.svg"
+                            />
+
+                            <div className="text-wrapper-10">Research</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="div-8">
+                      <div className="label-2">
+                        <div className="text-wrapper-11">Tags</div>
+                      </div>
+
+                      <div className="div-9">
+                        <div className="span-3">
+                          <div className="text-wrapper-12">Creative</div>
+                        </div>
+
+                        <div className="span-4">
+                          <div className="text-wrapper-13">Research</div>
+                        </div>
+
+                        <button className="button-2">
+                          <div className="text-wrapper-14">+ Add Tag</div>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="div-10">
+              <div className="div-11">
+                <button className="button-3">
+                  <div className="text-wrapper-15">Back</div>
+                </button>
+
+                <button className="button-4">
+                  <div className="text-wrapper-16">Next</div>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="div-12">
+            <div className="text-wrapper-17">SchoolHub</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
